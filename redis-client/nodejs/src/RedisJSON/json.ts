c1bd8e38268becefe53b4b3309bbac4673cfb86e
Angga Ari Wijaya <anggadarkprince@gmail.com>
2024-06-02 16:24:50 +0700

    feat(datatype): add update for json and string option

--- redis-client/nodejs/src/RedisJSON/json.ts
+++ redis-client/nodejs/src/RedisJSON/json.ts
@@ -5,7 +5,13 @@ const run = async () => {
     await client.del('restaurants');
 
     // JSON.SET profiles $ '"Hyperion"'
-    // JSON.SET restaurants:1 . '
+    // JSON.SET profiles . '"Hyperion"'
+
+    // Using $ Notation: When you use $ notation, you are specifying that the entire JSON object should be replaced with the new value.
+    // In this example, if profiles is a JSON object and you have a nested key like {"profiles": {"name": "John"}},
+    // using JSON.SET profiles . '"Hyperion"' would change the value of profiles to {"name": "John", "Hyperion": <value>}.
+
+    // JSON.SET restaurants:1 $ '
     // {
     //    "name: "Tacos Mi 45",
     //    "is_open": true,
@@ -15,7 +21,7 @@ const run = async () => {
     //        {"name": "Quesadilla", "price": 6.1}
     //    ]
     //
-    const restaurant1Set = await client.json.set('restaurants:1', '.', {
+    const restaurant1Set = await client.json.set('restaurants:1', '$', {
         name: 'Tacos Mi 45',
         is_open: true,
         location: {
@@ -36,27 +42,27 @@ const run = async () => {
 
     // JSON.GET restaurants:1 .menu
     // JSON.GET restaurants:1 .menu[0].price
-    const restaurant1Menu = await client.json.get('restaurants:1', {path: '.menu'});
+    const restaurant1Menu = await client.json.get('restaurants:1', {path: '$.menu'});
     console.log('Data restaurant 1 menu:', JSON.stringify(restaurant1Menu, null, 2));
-    const restaurant1MenuPrice = await client.json.get('restaurants:1', {path: '.menu[0].price'});
+    const restaurant1MenuPrice = await client.json.get('restaurants:1', {path: '$.menu[0].price'});
     console.log('Data restaurant 1 price:', restaurant1MenuPrice);
 
-    const updatedContact = await client.json.set('restaurants:1', '.contact', '05627234244');
+    const updatedContact = await client.json.set('restaurants:1', '$.contact', '05627234244');
     console.log('Data update contact:', updatedContact, JSON.stringify(await client.json.get('restaurants:1'), null, 2));
 
     // JSON.SET restaurants:1 .location.address "\"145 1st ave, oakland, CA 3332\""
-    const updateAddress = await client.json.set('restaurants:1', '.location.address', '145 1st ave, oakland, CA 3332');
+    const updateAddress = await client.json.set('restaurants:1', '$.location.address', '145 1st ave, oakland, CA 3332');
     console.log('Data update address:', updateAddress, JSON.stringify(await client.json.get('restaurants:1'), null, 2));
 
     // JSON.ARRAPPEND restaurants:1 .menu '{"name": "Torta", "price": 4.5}'
-    console.log('Append menu', await client.json.arrAppend('restaurants:1', '.menu', {"name": "Torta", "price": 4.5}))
+    console.log('Append menu', await client.json.arrAppend('restaurants:1', '$.menu', {"name": "Torta", "price": 4.5}))
 
     // JSON.ARRLEN restaurants .menu
-    console.log('Total menu:', await client.json.arrLen('restaurants:1', '.menu'))
+    console.log('Total menu:', await client.json.arrLen('restaurants:1', '$.menu'))
 
     // JSON.ARRPOP restaurants:1 .menu [0]
     // JSON.NUMINCRBY restaurants:1 .wait_time 10
-    await client.json.numIncrBy('restaurants:1', '.wait_time', 10)
+    await client.json.numIncrBy('restaurants:1', '$.wait_time', 10)
 
     console.log('Current data:', JSON.stringify(await client.json.get('restaurants:1'), null, 2));
 };
--- redis-client/nodejs/src/string.ts
+++ redis-client/nodejs/src/string.ts
@@ -10,5 +10,22 @@ const run = async () => {
     console.log(`${nameResult} - ${ageResult}`);
     console.log(await client.get('name'));
     console.log(await client.get('age'));
+
+
+    console.log('Remember me before?', await client.get('remember_me'));
+    const rememberMe = await client.set('remember_me', '1', {
+        EX: 3600 * 24 * 7, // expired in seconds
+        //PX: 3600 * 24 * 7 * 1000,   // Set the specified expire time, in milliseconds.
+        //EXAT: Date.now() + 60, // Set the specified Unix time at which the key will expire, in seconds.
+        //PXAT: Date.now() + 6000, // Set the specified Unix time at which the key will expire, in milliseconds.
+        //KEEPTTL: true, // Retain the TTL previously set on the key.
+
+        //NX: true, // Only set the key if it does not already exist.
+        //XX: true, // Only set the key if it already exists.
+
+        GET: true, // Return the old value stored at key, or nil when key did not exist.
+    });
+
+    console.log('Remember me', rememberMe);
 };
 run();
